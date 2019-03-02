514ba57a5880b57a34e2a82295346bd8ab109a50
Kristina Semenchikova <48157806+KristinaSemenchikova@users.noreply.github.com>
2019-03-02 19:22:39 +0300

    Add files via upload

---
+++ CV/page.js
@@ -0,0 +1,10 @@
+var run= 10;
+var calloriesPerRun= 15;
+var calloriesTotal = run + calloriesPerRun;
+alert (calloriesTotal);
+var admin = 'Василий';
+var name = 'Василий';
+document.write (admin);
+var earth = 'Земля';
+var namePetr = "Петя";
+
